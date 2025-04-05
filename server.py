f6b8919d7314a315d45cc5693eff2b603aa09aac
mads <maddy.siwick@icloud.com>
2025-04-04 23:10:30 -0400

    pushing code that doesnt work just so you have the 3d model

--- server.py
+++ server.py
@@ -1,3 +1,13 @@
-from flask import Flask
+from flask import Flask,jsonify
+import tarfile
+import tempfile
 
 app=Flask('__name__')
+
+@app.route('/')
+def server():
+#do i need a temp directory here and if i had one what would it do
+    tar=tarfile.open('sendtar.tar.gz','w:gz')
+    tar.add('Octoprint.usd')
+    #return tar
+    return jsonify({'message':'hello world!!!'})
